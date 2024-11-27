58dbd79e3dc777521ff89d1f7cd07c2351087148
olegchitaoleg <jaguar75oleg@gmail.com>
2024-11-27 23:22:17 +0900

    Add files via upload

---
+++ Task5.py
@@ -0,0 +1,8 @@
+total_task = 12
+hours_spend = 1.5
+course_name = 'Python'
+time_for_one_task = (hours_spend/total_task)
+print(time_for_one_task)
+print('Курс:',course_name, ',всего задач:',total_task, ',затрачено часов:' ,hours_spend, ',среднее время выполнения:',time_for_one_task )
+
+
